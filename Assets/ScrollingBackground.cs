882cfc8047fc31d22749045a7119dc55afb54fc8
kekresna <alvinkresna@gmail.com>
2016-11-01 00:24:52 +0700

    Trex+Scrolling

---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float speed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+
+		GetComponent<Renderer>().material.mainTextureOffset = offset;﻿
+	}
+}
